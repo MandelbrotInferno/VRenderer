082479948c1961328bda2540e40700aa4474913c
Farhan Aghnoum <130616138+MandelbrotInferno@users.noreply.github.com>
2025-06-29 07:25:58 +0430

    Add new scope to VULKAN_ERROR
    The reason this is needed is to avoid redefinition of err. We might use this macro in the same scope multiple times and so in order to avoid redefining err multiple times this addition of new scope is necessary.

--- include/VulkanError.hpp
+++ include/VulkanError.hpp
@@ -7,10 +7,13 @@
 
 namespace VRenderer
 {
+
 #define VULKAN_CHECK(x)                                                     \
-        VkResult err = x;                                               \
-        if (err) {                                                      \
-            fmt::print("Detected Vulkan error at line: {0}\nFile: {1}\nVulkan Error: {3}\n",__LINE__, __FILE__, string_VkResult(err),); \
-            throw "Vulkan error occured. Aborting by throwing exception.";                                                    \
-        }
+        {\
+    VkResult err = x;                                               \
+        if (err) {\
+                fmt::print("Detected Vulkan error at line: {0}\nFile: {1}\nVulkan Error: {2}\n", __LINE__, __FILE__, string_VkResult(err)); \
+                throw "Vulkan error occured. Aborting by throwing exception.";                                                    \
+        }\
+    }
 }
