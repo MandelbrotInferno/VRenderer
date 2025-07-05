5710a5ee425e8e0736f8ba2365988e3ac01f195f
Farhan Aghnoum <130616138+MandelbrotInferno@users.noreply.github.com>
2025-07-05 13:17:28 +0430

    Start implementing vk resource manager

---
+++ include/VulkanResourceManager.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+
+
+#include "include/VulkanTexture.hpp"
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+namespace VRenderer
+{
+	class VulkanResourceManager final
+	{
+	public:
+
+		//Make sure every name you provide is unique
+		void AddVulkanTexture(std::string&& l_name, VulkanTexture&& l_vulkanTexture);
+
+		//Make sure every name you provide is unique
+		void AddVulkanImageView(std::string&& l_name, VkImageView l_vulkanImageView);
+
+		VulkanTexture& RetrieveVulkanTexture(std::string_view l_name);
+		VkImageView RetrieveVulkanImageView(std::string_view l_name);
+
+	private:
+		std::vector<VulkanTexture> m_vulkanTextures{};
+		std::vector<VkImageView> m_vulkanImageViews{};
+		std::unordered_map<std::string, size_t> m_mapVulkanTextureNamesToIndex{};
+		std::unordered_map<std::string, size_t> m_mapVulkanImageViewsNamesToIndex{};
+	};
+}
---
+++ src/VulkanResourceManager.cpp
@@ -0,0 +1,48 @@
+
+
+
+
+
+#include "include/VulkanResourceManager.hpp"
+
+
+namespace VRenderer
+{
+	void VulkanResourceManager::AddVulkanTexture(std::string&& l_name, VulkanTexture&& l_vulkanTexture)
+	{
+		m_vulkanTextures.emplace_back(std::move(l_vulkanTexture));
+		const size_t lv_index = m_vulkanTextures.size();
+		m_mapVulkanTextureNamesToIndex.emplace(std::move(l_name), lv_index);
+	}
+
+	void VulkanResourceManager::AddVulkanImageView(std::string&& l_name, VkImageView l_vulkanImageView)
+	{
+		m_vulkanImageViews.push_back(l_vulkanImageView);
+		const size_t lv_index = m_vulkanImageViews.size();
+		m_mapVulkanImageViewsNamesToIndex.emplace(std::move(l_name), lv_index);
+	}
+
+	VulkanTexture& VulkanResourceManager::RetrieveVulkanTexture(std::string_view l_name)
+	{
+		const std::string lv_tempName{ l_name };
+		auto lv_iter = m_mapVulkanTextureNamesToIndex.find(lv_tempName);
+
+		if (m_mapVulkanTextureNamesToIndex.end() != lv_iter) {
+			return m_vulkanTextures[lv_iter->second];
+		}
+		
+		throw "Requested VulkanTexture was not found in the vulkan resource manager.\n";
+	}
+
+	VkImageView VulkanResourceManager::RetrieveVulkanImageView(std::string_view l_name)
+	{
+		const std::string lv_tempName{ l_name };
+		auto lv_iter = m_mapVulkanImageViewsNamesToIndex.find(lv_tempName);
+
+		if (m_mapVulkanTextureNamesToIndex.end() != lv_iter) {
+			return m_vulkanImageViews[lv_iter->second];
+		}
+
+		throw "Requested VkImageView was not found in the vulkan resource manager.\n";
+	}
+}
