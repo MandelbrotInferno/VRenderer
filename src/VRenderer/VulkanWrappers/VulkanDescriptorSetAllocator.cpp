69a8f9b435ecbffb783c5c9e0decea41a76ecf03
Farhan Aghnoum <130616138+MandelbrotInferno@users.noreply.github.com>
2025-08-08 11:04:02 -0400

    Improve error handling in desc set allocator

--- src/VRenderer/VulkanWrappers/VulkanDescriptorSetAllocator.cpp
+++ src/VRenderer/VulkanWrappers/VulkanDescriptorSetAllocator.cpp
@@ -43,16 +43,21 @@ namespace VRenderer
 		VkResult lv_result{};
 		lv_result = vkAllocateDescriptorSets(l_device, &lv_allocateInfo, &lv_set);
 		if (VK_ERROR_OUT_OF_POOL_MEMORY == lv_result) {
-			auto lv_pool = Utilities::GenerateVkDescriptorPool(l_device, m_descriptorPollSizes, m_maxNumSetsAllowedPerPool);
-			m_pools.push_back(lv_pool);
-			++m_currentPoolIndex;
-			m_totalNumSetAllocFromCurrentPool = 0U;
-			lv_result = vkAllocateDescriptorSets(l_device, &lv_allocateInfo, &lv_set);
-			if (VK_ERROR_OUT_OF_POOL_MEMORY == lv_result) {
+			if (0U == m_totalNumSetAllocFromCurrentPool) {
 				throw "Total number of at least one of the specified descriptor types in the descirptor set layout exceeds the total number of that the pool has of that type.\n";
 			}
 			else {
-				VULKAN_CHECK(lv_result);
+				auto lv_pool = Utilities::GenerateVkDescriptorPool(l_device, m_descriptorPollSizes, m_maxNumSetsAllowedPerPool);
+				m_pools.push_back(lv_pool);
+				++m_currentPoolIndex;
+				m_totalNumSetAllocFromCurrentPool = 0U;
+				lv_result = vkAllocateDescriptorSets(l_device, &lv_allocateInfo, &lv_set);
+				if (VK_ERROR_OUT_OF_POOL_MEMORY == lv_result) {
+					throw "Total number of at least one of the specified descriptor types in the descirptor set layout exceeds the total number of that the pool has of that type.\n";
+				}
+				else {
+					VULKAN_CHECK(lv_result);
+				}
 			}
 		}
 		else {
