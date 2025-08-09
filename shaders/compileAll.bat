09087a7c863a55b57489c7c97a855b537c8b7d94
Farhan Aghnoum <130616138+MandelbrotInferno@users.noreply.github.com>
2025-08-09 05:15:40 -0400

    Restructure shader folder

--- shaders/compileAll.bat
+++ shaders/compileAll.bat
@@ -1,28 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Path to glslangValidator (adjust if not in PATH)
-set GLSLANG_VALIDATOR=glslangValidator
+REM Change this if glslangValidator is not in PATH
+set GLSLANG=glslangValidator.exe
 
-:: Output directory for compiled SPIR-V shaders
-set OUTPUT_DIR=SPIRV-CompiledShaders
-
-:: Create output directory if it doesn't exist
-if not exist "%OUTPUT_DIR%" (
-    mkdir "%OUTPUT_DIR%"
-)
-
-:: List of shader extensions to compile
-for %%F in (*.vert *.frag *.comp *.geom *.tesc *.tese) do (
-    echo Compiling %%F ...
-    %GLSLANG_VALIDATOR% -V "%%F" -o "%OUTPUT_DIR%\%%~nF.spv"
-    if errorlevel 1 (
-        echo Error compiling %%F
-    ) else (
-        echo Successfully compiled %%F to %OUTPUT_DIR%\%%~nF.spv
+for /D %%D in (*) do (
+    if exist "%%D\SPV" (
+        echo Found SPV folder in %%D
+        for %%F in ("%%D\*.vert" "%%D\*.frag" "%%D\*.comp" "%%D\*.geom" "%%D\*.tesc" "%%D\*.tese") do (
+            if exist "%%F" (
+                set "FILENAME=%%~nF"
+				echo.
+                echo Compiling %%F to %%D\SPV\!FILENAME!.spv
+                "%GLSLANG%" -V "%%F" -o "%%D\SPV\!FILENAME!.spv"
+				echo.
+            )
+        )
     )
 )
 
-echo.
-echo All shaders processed.
+echo Done.
 pause
+
